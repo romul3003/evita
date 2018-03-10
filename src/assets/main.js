ff3bb7605daa0b11d5d6fa52be62a241d21340df
Azrton <toshauiux@gmail.com>
2018-03-10 20:16:52 +0200

    Add slick slider

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,11 +1,28 @@
 $( document ).ready(function() {
-<<<<<<< HEAD
 	console.log('jQuery is ready!');	
 });
 
 
 $(document).ready(function(){
-  $('.oursalons__myslick').slick();
+
+    var $status = $('.pagingInfo');
+    var $slickElement = $('.oursalons__myslick');
+
+    $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $status.text(i + '/' + slick.slideCount);
+        console.log(i);
+        console.log(currentSlide);
+        $('.oursalons__item1').removeClass('oursalons__select'); 
+        $('.oursalons__item2').removeClass('oursalons__select'); 
+        $('.oursalons__item3').removeClass('oursalons__select'); 
+        $('.oursalons__item'+i).addClass('oursalons__select'); 
+
+
+    });
+
+    $slickElement.slick();
+
 
 	console.log('jQuery is ready!');
 	$('.header__burger').click(function(e) {
