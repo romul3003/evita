c6eb42766c14970717ece821fda7e4eb05ce5a2d
Roman Zakrevskyi <romul3003@gmail.com>
2018-03-10 18:30:08 +0200

    made structure, styles of feedback-form, installed and added sass compilation in gulpfile.js, added select-2 styles in sass directory

--- src/blocks/feedback-form/feedback-form.js
+++ src/blocks/feedback-form/feedback-form.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    $('.feedback-form__form-control_select').select2();
+    $('.feedback-form__form-control_select').select2({
+    	placeholder: 'Выберете салон',
+    	allowClear: true,
+    	minimumResultsForSearch: Infinity,
+    	theme: "default"
+    });
 });
