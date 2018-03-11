87c27eec923cb392ba4f7c8e3eed2d9a054b1ba4
Azrton <toshauiux@gmail.com>
2018-03-11 10:39:04 +0200

    Update slick slider

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,24 +5,44 @@ $( document ).ready(function() {
 
 $(document).ready(function(){
 
-    var $status = $('.pagingInfo');
     var $slickElement = $('.oursalons__myslick');
 
     $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
         var i = (currentSlide ? currentSlide : 0) + 1;
-        $status.text(i + '/' + slick.slideCount);
-        console.log(i);
-        console.log(currentSlide);
         $('.oursalons__item1').removeClass('oursalons__select'); 
         $('.oursalons__item2').removeClass('oursalons__select'); 
         $('.oursalons__item3').removeClass('oursalons__select'); 
-        $('.oursalons__item'+i).addClass('oursalons__select'); 
-
-
+        $('.oursalons__item'+i).addClass('oursalons__select');
+        console.log(i);
+        if(i==1) {
+        	$('.oursalons__lines').css('margin-left', '0px');
+        	console.log('Uiiii');
+        	$('.oursalons__titlem').html('Evita Premium Ходынка');
+        }
+        if(i==2) {
+        	$('.oursalons__lines').css('margin-left', '29px');
+        	console.log('Uiiii');
+        	$('.oursalons__titlem').html('Evita classic');
+        }
+        if(i==3) {
+        	$('.oursalons__lines').css('margin-left', '56px');
+        	console.log('Uiiii');
+        	$('.oursalons__titlem').html('Evita studio речной');
+        }
     });
 
-    $slickElement.slick();
+    $slickElement.slick({
+    	dots: true,
+    	dotsClass: 'custom-dots',
+		customPaging: function(slick,i) {
+			var i=i+1;
+			var titles=$('.oursalons__item'+i).data('title')
+        	return '<a>' + titles + '</a>';
+    	}
+    });
 
+    $('.slick-next.slick-arrow').html('');
+    $('.slick-prev.slick-arrow').html('');
 
 	console.log('jQuery is ready!');
 	$('.header__burger').click(function(e) {
