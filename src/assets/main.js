95272c34829d21f34101f48e1a9554dc9b388c77
Roman Vabishchevych <roma@se7ensky.com>
2018-03-12 20:05:11 +0200

    new timelines

--- src/assets/main.js
+++ src/assets/main.js
@@ -11,56 +11,26 @@ $( document ).ready(function() {
 
 $(".accordion-btn").each(function(index, element){
 
-    //this introduces child elements in sequence
+    var $this = $(element);
+    var isOpen = false;
 
-    var introAnimation = new TimelineLite({paused:true});
-    introAnimation.staggerFrom(".accordion-panel" + index + " ul", 0.5, {y:'-=50', opacity:0}, .1);
+    var openAnimation = new TimelineMax({paused:true})
+        .from($this.find(".accordion-panel"), 1, {height:0})
+        .staggerFromTo($this.find(".accordion-panel ul"), 0.5, {y: -50, autoAlpha: 0}, {y: 0, autoAlpha: 1}, .1);
 
-    //this animation opens or closes the accordion-panel
-    element.openAnimation = TweenMax.from(".accordion-panel" + index, 1,{height:0, paused:true, onReverseComplete:resetIntro, onReverseCompleteScope:this});
+    var closeAnimation = new TimelineMax({paused:true})
+        .staggerTo($this.find(".accordion-panel ul"), 0.5, {y: -50, autoAlpha: 0}, .1)
+        .to($this.find(".accordion-panel"), 1, {height:0});
 
-    element.introAnimation = introAnimation;
-    element.index = index;
-    if(element.index === 0) {
-        element.open = true;
-        this.openAnimation.play();
-        TweenLite.delayedCall(0.3, playIntro, null, this);
-    } else {
-        element.open = false;
-    }
-    console.log(element.openAnimation);
-    //this gets triggered by openAnimation's onReverseCompleteCallback so that things get reset when accordion-panel closes
-    function resetIntro() {
-        this.introAnimation.pause(0);
-    }
-
-
-    function playIntro() {
-        //if the introAnimation is partially revealed then keep playing it forward
-        // this condition is necessary if people click open / close very quickly.
-        if(this.introAnimation.progress() > 0){
-            this.introAnimaiton.play()
+    $this.click(function(){
+        if(!isOpen){
+            closeAnimation.pause();
+            openAnimation.play();
+            isOpen = true;
         } else {
-            this.introAnimation.restart();
-        }
-    }
-
-    //figure out what to do on each click based on whether or not accordion-panel is open or closed
-
-    $(this).click(function(){
-
-        if(!this.open){
-            this.openAnimation.play();
-            //0.3 seconds after telling the open animation to play, call a function that will play the introAnimation
-            TweenLite.delayedCall(0.3, playIntro, null, this);
-            this.open = true;
-            console.log('open' + this)
-        } else {
-            //accordion-panel is open, lets close it
-            this.openAnimation.reverse();
-            this.open = false;
-            console.log('close' + this)
-
+            openAnimation.pause();
+            closeAnimation.play();
+            isOpen = false;
         }
     });
 });
