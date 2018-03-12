e109bf667fb9234c03541739c16a3a7619a59084
Roman Zakrevskyi <romul3003@gmail.com>
2018-03-12 11:46:36 +0200

    added feedback-form styles and structure, changed select-2 styles

--- src/blocks/feedback-form/feedback-form.js
+++ src/blocks/feedback-form/feedback-form.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.feedback-form__form-control_select').select2({
-    	placeholder: 'Выберете салон',
+    	placeholder: 'Выберете салон*',
     	allowClear: true,
     	minimumResultsForSearch: Infinity,
     	theme: "default"
