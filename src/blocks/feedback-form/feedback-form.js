e3a99abb3ccb28e26b3c26311329a8c4a1baa674
Roman Zakrevskyi <romul3003@gmail.com>
2018-03-15 14:12:06 +0200

    made fixes in feedback_form

--- src/blocks/feedback-form/feedback-form.js
+++ src/blocks/feedback-form/feedback-form.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-    $('.feedback-form__form-control_select').select2({
-    	placeholder: 'Выберете салон*',
+	var placeholder =  
+		$(".feedback-form__form-control_select").data("placeholder");
+
+    $(".feedback-form__form-control_select").select2({
+    	placeholder: placeholder,
     	allowClear: true,
     	minimumResultsForSearch: Infinity,
     	theme: "default"
